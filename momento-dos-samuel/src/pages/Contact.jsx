0b691f877f12683cc1b6565f3732c5bab49e992e
samuel martinez hoyos <samuel.martinez200730@gmail.com>
2026-04-22 07:44:41 -0500

    implementar formulario con almacenamiento en localStorage

---
+++ momento-dos-samuel/src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Form from "../components/Form";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <h2>Contacto</h2>
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
