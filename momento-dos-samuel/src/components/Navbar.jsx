816a18e7437a9b4c945e1fc8ffc03404a5959786
samuel martinez hoyos <samuel.martinez200730@gmail.com>
2026-04-22 08:49:21 -0500

    ok

--- momento-dos-samuel/src/components/Navbar.jsx
+++ momento-dos-samuel/src/components/Navbar.jsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2>ZENITH BONSAI</h2>
+
+      {/* 👇 ESTE ES EL CAMBIO */}
+      <h2>
+        <Link to="/">ZENITH BONSAI</Link>
+      </h2>
 
       <ul>
-        <li><Link to="/filosofia">Filosofía</Link></li>
-        <li><Link to="/gallery">Galería</Link></li>
-        <li><Link to="/contact">Reservas</Link></li>
+        <li>
+          <Link to="/filosofia">Filosofía</Link>
+        </li>
+
+        <li>
+          <Link to="/gallery">Galería</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Reservas</Link>
+        </li>
       </ul>
+
     </nav>
   );
 }
